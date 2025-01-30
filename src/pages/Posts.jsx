43a9631cd11fd3549c7fad6979d0944f70cf39b6
MeetBagda <bagdameet@gmail.com>
2025-01-30 23:58:41 +0530

    modify post.jsx

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
-import * as Heroicons from '@heroicons/react/24/outline';
+import { Avatar } from "@mui/material";
+import VerifiedUserIcon from '@mui/icons-material/VerifiedUser';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import RepeatIcon from '@mui/icons-material/Repeat';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import PublishIcon from '@mui/icons-material/Publish';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import  BookmarkBorder  from '@mui/icons-material';
+
 
 const Posts = ({name, username, post, photo, profilePhoto}) => {
     return (
         <div className="border rounded-md p-4 bg-white shadow-sm mb-4">
             <div className="flex items-start space-x-2">
-               <img src={profilePhoto}
-                    alt="avatar" className="h-10 w-10 rounded-full object-cover"/>
+               <Avatar src={profilePhoto}
+                    alt="avatar"  />
             <div className="flex flex-col">
                     <div className="flex items-center space-x-1">
                         <h3 className="font-medium text-gray-700">{name}</h3>
-                       <Heroicons.CheckCircleIcon className="h-4 w-4 inline-block fill-blue-500"/>
-                        <span className="text-gray-400">@{username}</span>
+                           <CheckCircleIcon className="h-4 w-4 inline-block fill-blue-500" style={{ fontSize: '15px' }}/>
+                         <span className="text-gray-400">@{username}</span>
 
                      </div>
                 <p>{post}</p>
@@ -25,11 +33,11 @@ const Posts = ({name, username, post, photo, profilePhoto}) => {
 
           }
            <div className="flex items-center justify-between mt-4">
-             <Heroicons.ChartBarIcon className="post_footer_icon"/>
-              <Heroicons.AcademicCapIcon className="post_footer_icon"/>
-                <Heroicons.AdjustmentsHorizontalIcon className="post_footer_icon"/>
-                <Heroicons.ArchiveBoxIcon className="post_footer_icon"/>
-                <Heroicons.AdjustmentsVerticalIcon className="post_footer_icon"/>
+                <ChatBubbleOutlineIcon  style={{ fontSize: '20px' }}/>
+                <RepeatIcon  style={{ fontSize: '20px' }}/>
+               <FavoriteBorderIcon  style={{ fontSize: '20px' }}/>
+               <BookmarkBorder  style={{ fontSize: '20px' }}/>
+                <PublishIcon  style={{ fontSize: '20px' }}/>
             </div>
         </div>
     );
